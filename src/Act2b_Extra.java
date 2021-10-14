3b5da838d93b7651861d0fc0c754e208ae8f862c
Ken <ken_tamck@hotmail.com>
2021-10-14 11:08:25 +0800

    added act2_extra

---
+++ src/Act2b_Extra.java
@@ -0,0 +1,15 @@
+import java.util.Calendar;
+import java.io.Console;
+
+class UserProfile_2 {
+    public static void main(String[]args){
+        Console console = System.console();
+        System.out.println("Please enter your name: ");
+        String userName = console.readLine();
+        System.out.println("Please enter your age: ");
+        int age = Integer.parseInt(System.console().readLine());
+        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+        int YOB = currentYear - age;
+        System.out.println("Hi, " + userName + ", age " + age + ". You are born in the year, " + YOB + "!");
+        }
+}
